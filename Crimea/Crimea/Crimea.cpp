36ac39e628d51197b3b68a21de1fc32df8826110
DFooRS <dima.lutsenko03@gmail.com>
2022-10-01 12:17:53 +0300

    Добавлено сообщение о начале и завершении аннексии

--- Crimea/Crimea/Crimea.cpp
+++ Crimea/Crimea/Crimea.cpp
@@ -6,12 +6,16 @@ int percent = 0;
 int main()
 {
     setlocale(0, "");
+    cout << "Аннексия Крыма начата" << endl << "..." << endl;
+    Sleep(1000);
     while (percent < 100)
     {
         cout << "Аннексия Крыма завершена на " << percent << " процентов" << endl << "..." << endl;
         percent += 10;
         Sleep(1000);
     }
+    cout << "Аннексия Крыма успешно завершена. Поздравляем" << endl << "..." << endl;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 }
 
 
